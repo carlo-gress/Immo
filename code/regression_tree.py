918e41e2d69c5aed3c1cdbbd7a623acd853fb217
S. S <44981224+odros@users.noreply.github.com>
2022-04-12 23:50:16 +0200

    Update regression_tree.py

--- code/regression_tree.py
+++ code/regression_tree.py
@@ -27,17 +27,17 @@ data = data.drop('adat', axis = 1)
 # Join the encoded df
 data = data.join(one_hot_adat)
 
-# Prepare 'X' and 'y'
+## Prepare 'X' and 'y'
 X = data.loc[:, data.columns != 'hits']
 y = data.loc[:, 'hits']
 
-# Split the data
+## Split the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
-# Train
+## Train
 tree_reg = DecisionTreeRegressor()
 tree_reg.fit(X_train, y_train)
 
-# 
+## Predict and evaluate 
 y_predictions = tree_reg.predict(X_test)
 tree_mse = mean_squared_error(y_test, y_predictions)
